c83db166ad8e59f106d40398c227618713623982
yuuhekurenai <yuuhekurenai@gmail.com>
2023-02-06 11:01:36 -0300

    App do projeto foi separado para uma melhor organização e visualização do codigo e das pastas.

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+def projects(request):
+    return HttpResponse('Aqui está o primeiro projeto')
+
+
+def project(request, pk):
+    return HttpResponse('Aqui está o primeiro projeto unico' + ' ' + str(pk))
+
