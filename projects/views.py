278708b01bc52727523b7d7406698d0a33cfbce3
yuuhekurenai <yuuhekurenai@gmail.com>
2023-02-07 11:36:16 -0300

    Acessando páginas de forma dinâmica

--- projects/views.py
+++ projects/views.py
@@ -1,13 +1,38 @@
-from django.shortcuts import render # render recebe a informação passado no settings
-#sobre a pasta templates, então agora podemos referencia-la utilizando return render.
+from django.shortcuts import render
+# render recebe a informação passado no settings
+# sobre a pasta templates, então agora podemos referenciá-la utilizando return render.
 
 from django.http import HttpResponse
 
+ProjectList = [
+    {
+        'id': '1',
+        'title': "Ecommerce Website",
+        'description': 'Fully functional ecommerce website',
+    },
+    {
+        'id': '2',
+        'title': "Portfolio Website",
+        'description': 'This was a project where I built out my portifolio',
+    },
+    {
+        'id': '3',
+        'title': "Social Network",
+        'description': ' Awesome open source project I am still working on'
+    },
+]
+
 
 def projects(request):
-    return render(request, 'projects/project.html')
+    page = 'Project'
+    number = 8
+    context = {'page': page, 'number': number, 'projects': ProjectList}
+    return render(request, 'projects/projects.html', context)
 
 
 def project(request, pk):
-    return render(request, 'projects/single-project.html')
-
+    ProjectObj = None
+    for i in ProjectList:
+        if i['id'] == pk:
+            ProjectObj = i
+    return render(request, 'projects/single-project.html', {'project': ProjectObj})
