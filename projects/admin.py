a4f22196fd2f87b18443db7c67e947056b6c9fb6
yuuhekurenai <yuuhekurenai@gmail.com>
2023-02-07 14:36:39 -0300

    1° Model criado / migrações feitas / Painel atualizado.

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+# Aqui é possível adicionar e atualizar os modelos do Admin Panel.
+admin.site.register(Project) # Não esquecer de colocar admin.site para registrar o novo modelo.
--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+import uuid
+# import da library uuid como garantia de que o id não seja repetido.
 
-# Create your models here.
+
+# A modelagem do banco de dados utilizando models do Django.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    demo_link = models.CharField(max_length=2000, blank=True)
+    source_code = models.CharField(max_length=2000, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.title
